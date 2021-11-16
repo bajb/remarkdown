6de462218e3066bc64974015bc50a66c77b31181
Brooke Bryan <brooke@bajb.net>
2021-11-16 13:53:43 +0000

    Add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b283aa393111f2ea14302ddd4acfab185e5beac1',
+        'name' => 'packaged/remarkdown',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'packaged/remarkdown' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b283aa393111f2ea14302ddd4acfab185e5beac1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
